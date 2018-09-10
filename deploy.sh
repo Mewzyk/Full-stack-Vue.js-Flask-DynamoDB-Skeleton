87c99ce344ffd1a892ca8482f36d14b68a75bca7
Stephen Thomas <stephen.thomas@dovetail-genomics.com>
2018-09-09 19:09:17 -0700

    Added index.html and deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo ####################
+echo # Start Deployment #
+echo ####################
+
+aws s3 sync ../ s3://views-with-vue/ --delete
+
+echo ####################
+echo # End Deployment   #
+echo ####################
